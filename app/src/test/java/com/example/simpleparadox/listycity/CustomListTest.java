47af217f5879dd987c1829ac1daaa4a61f561ab5
Andrei Bagui <andreibagui@outlook.com>
2021-11-01 09:31:20 -0600

    RED: Added hasCity test (should fail)

--- app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CustomListTest.java
@@ -1,6 +1,8 @@
 package com.example.simpleparadox.listycity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -20,5 +22,14 @@ public class CustomListTest {
         int listSize = list.getCount();
         list.addCity(new City("Edmonton","Alberta"));
         assertEquals(list.getCount(), listSize+1);
+    }
+
+    @Test
+    void testHasCity(){
+        City charlottetown = new City("Charlottetown", "Prince Edward Island");
+        list.addCity(charlottetown);
+        assertTrue(list.hasCity(charlottetown));
+        City toronto = new City("Toronto", "Ontario");
+        assertFalse(list.hasCity(toronto));
     }
 }
